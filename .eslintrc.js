1e62ddee49f1990e01f701c3fcbef3576d6d791a
Abner Persio <binhopersio@gmail.com>
2022-10-01 12:09:40 -0300

    chore: ud eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,15 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
-    'jest/globals': true,
+    jest: true,
   },
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
     project: 'tsconfig.json',
+    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'jest'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
@@ -19,25 +18,10 @@ module.exports = {
     'eslint-config-prettier',
   ],
   rules: {
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
-    'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error',
-    'import/extensions': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/camelcase': 'off',
     'import/prefer-default-export': 'off',
     'class-methods-use-this': 'off',
-    'no-underscore-dangle': 'off',
-    'operator-linebreak': 'off',
-    'object-curly-newline': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'max-len': 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {},
-    },
+    'no-useless-constructor': 'off',
   },
 };
